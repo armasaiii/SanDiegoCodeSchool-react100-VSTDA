03846cc31af6638612397ea95d2021e171d6f17c
Armando Armas III <armando.armas@sdcs.us>
2020-01-29 13:26:05 -0800

    SanDiegoCodeSchool-react100-VSTDA
    This is a work in progress.
    I created an additional component <ToDoList/>
    My JSX is now in the <ToDoList/> 
    My constructor and the beginning of my functions are in <ToDos/>

--- App.jsx
+++ App.jsx
@@ -1,44 +1,10 @@
 import React, { Component } from 'react';
 import ToDos from './ToDos'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      ToDos: []
-    }
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick() {
-    console.log('NICE!!!')
-    this.setState ({ToDos: 'I am doing fine, Thank you.'})
-  }
-
-
-
-  render() {
-    // const ToDos = this.state.ToDos.map(item => <ToDos handleClick={this.handleClick}/>)
-    // let wordDisplay
-    // if(this.state.test === true) {
-    //   wordDisplay = 'F YEAH!!!!'
-    // } else {
-    //   wordDisplay = 'WTF?!?!?!'
-    // }
-
-    return (
-      <div>
-        {/* <button onClick={this.handleClick}>TRY ME</button> */}
-        {/* <h1>{wordDisplay}</h1> */}
-      
-        <ToDos/>
-    
-
-        
-        <p>{this.state.ToDos}</p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <ToDos/>
+  )
 }
 
-export default App;
+export default App
--- ToDos.jsx
+++ ToDos.jsx
@@ -1,50 +1,27 @@
 import React from 'react'
+import ToDoList from './ToDoList'
 
- export default props => (
-    <div>
-        <div className='header'>
-            <header>
-                <h1>Very Simple To Do App</h1>
-                    <h3>Track all of the things I have to do</h3>
-                <hr/>
-            </header>
-        </div>
-        <div className='container-input'>
-        <div className='panel-body'> I need to</div>
-            <input className='textarea' type='text'/>
-                <br/>
-                <select className='priority'>
-                    <option className='priority' value='priority'>Priority</option>
-                    <option className='priority1' value='high'>High</option>
-                    <option className='priority2' value='normal'>Normal</option>
-                    <option className='priority3' value='low'>Low</option>
-                </select>
-            <div className='submit'>
-                <button type='submit'>Add</button>
+class ToDos extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        ToDos: []
+      }
+      this.handleClick = this.handleClick.bind(this)
+    }
+  
+    handleClick() {
+      console.log('NICE!!!')
+      this.setState ({ToDos: 'I am doing fine, Thank you.'})
+    }
+
+    render () {
+        return (
+            <div>
+                <ToDoList/>
             </div>
-        </div>
+        )
+    }
+}
 
-        <div className='welcome-section'>
-            <header className='panel-body1'>
-                {/* <h1>{this.state.test}</h1> */}
-                <h4 className='topper'>View ToDos</h4>
-                    <h5 className='topper-inst'>Welcome to a very simple to do App!</h5>
-                        <p className='topper-inst'>Get started now by adding a new ToDo on the list.</p>
-            </header>
-            
-        </div>
-
-        <div className='container-todo'>
-            <input type="checkbox" />
-                <p>Get up</p>
-            <input type="checkbox" />
-                <p>Exercise</p>
-            <input type="checkbox" />
-                <p>Code</p>
-            <input type="checkbox" />
-                <p>Karaoke</p>
-        </div>
-    </div>
-      )
-    
-
+export default ToDos
