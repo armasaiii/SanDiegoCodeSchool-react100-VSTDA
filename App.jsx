df59019216f0c68130b6521610896916b3b54ef2
Armando Armas III <armando.armas@sdcs.us>
2020-02-19 12:05:26 -0800

    SanDiegoCodeSchool-react100-VSTDA
    SanDiegoCodeSchool-react100-VSTDA
    Working VSTDA App made with React

--- App.jsx
+++ App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import ToDo from './ToDo'
 import ToDoList from './ToDoList'
-import List from './List'
+// import EditComponent from './EditComponent'
+// import SaveComponent from './SaveComponent'
 
 class App extends React.Component {
   constructor(props) {
@@ -8,80 +10,162 @@ class App extends React.Component {
 
     this.state = {
       list: [],  
-      newTask: '',
-      // delete:'',
-      // edit: '',
+      text: '',
+      priority: '',
+      newPriority: '',
+      newText: '',
+      isComplete: false,
     }
-    this.handleItemInput = this.handleItemInput.bind(this)
-    this.newItemSubmitHandler = this.newItemSubmitHandler.bind(this)
-    this.handleRemove = this.handleRemove.bind(this)
-    this.handleEdit = this.handleEdit.bind(this)
-    this.handlePriority = this.handlePriority.bind(this)
-
-
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+    this.handleViewTodoDelete = this.handleViewTodoDelete.bind(this)
+    this.handleEditChanges = this.handleEditChanges.bind(this)
+    this.handleSave = this.handleSave.bind(this)
+    this.clickEdit = this.clickEdit.bind(this)
+    this.clickDelete = this.clickDelete.bind(this)
+    this.handleNewSave = this.handleNewSave.bind(this)
+    this.viewTodoEdit = this.viewTodoEdit.bind(this)
+    this.viewToDoPriority = this.viewToDoPriority.bind(this)
+    this.completedTask=this.completedTask.bind(this)
+    // this.handleEdit = this.handleEdit.bind.this
   }
 
-  handleItemInput(event) {
-    this.setState({ 
-      newTask: event.target.value 
+  handleClick() {
+    const newTodo = {
+      text: this.state.text,
+      priority: this.state.priority,
+      // id: Date.now(),
+      isEditing: false
+    }
+    const newArray = [...this.state.list]
+    newArray.push(newTodo)
+    this.setState({
+      list: newArray
     })
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleEditChanges(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSave(id, text, priority) {
+    const newArray = [...this.state.list]
+    const tempId = id
+    const index = newArray.findIndex(item => item.id === tempId)
+    newArray[index].isEditing = false
+    newArray[index].text = text
+    newArray[index].priority = priority
+    this.setState({
+      list: newArray
+    })
+  }
+
+  handleEdit(id) {
+    const newArray = [...this.state.list]
+    const tempId = id
+    const index = newArray.findIndex(item => item.id === tempId)
+    newArray[index].isEditing = true
+    this.setState({
+      list: newArray
+    })
+  }
+
+  handleViewTodoDelete(id) {
+    const newArray = [...this.state.list]
+    const tempId = id
+    const index = newArray.findIndex(item => item.id === tempId)
+    newArray.splice(index, 1)
+    this.setState({
+      list: newArray
+    })
+  }
+
+  clickEdit() {
+    this.handleEdit(this.id)
+  }
+
+  clickDelete() {
+    this.handleViewTodoDelete(this.id)
+  }
+
+  handleNewSave() {
+    this.handleSave(this.state.list.id, this.state.newText, this.state.newPriority)
+    //might have to change this.state.newText and this.state.pro
+  }
+
+  viewTodoEdit(event) {
+    this.setState({newText: event.target.value})
   }
   
-  newItemSubmitHandler(event) {
-    event.preventDefault()
-    this.setState({
-      list: [
-        {
-          name: this.state.newTask,
-        },
-        ...this.state.list
-      ],
-      newTask: ''
-    })
+  viewToDoPriority(event) {
+    this.setState({newPriority: event.target.value})
   }
 
-  handleRemove(index) {
-   // console.log('nice', index)
-    event.preventDefault()
-    const newState = this.state.list.filter(item => {
-      this.state.list.indexOf(item) !== index
-    })
-
-    this.setState({ 
-      list: newState
-    })
-  }
-
-  handleEdit() {
-    this.setState = ({edit: this.state.edit})
-  }
-
-  handlePriority() {
-    this.setState = ({priority: this.state.priority})
-  }
-
-
+  completedTask() {
+  const {isComplete}=this.state
+  this.setState({isComplete: !isComplete})
+ }
 
   render() {
+    // const view = this.state.mode === 'view'
     return (
-      <div className='wrapper'>
-        <ToDoList
-          className='input'
-          type='text'
-          handleItemInput={this.handleItemInput}
-          newItemSubmitHandler={this.newItemSubmitHandler}
-          value={this.state.new}
-          placeholder='Add an item'
-        />
-        
-        <List 
-          list={this.state.list} 
-          handleRemove={this.handleRemove}
-        />
-        
+      <div className='container'>
+      <h1>Very Simple Todo App</h1>
+      <h2>Track all of the things</h2>
+      <hr />
+      <div className='form-group '>
+        <div className='row' >
+          <div className='form col-md-5'>
+            <ToDo
+              handleChange={this.handleChange}
+              handleClick={this.handleClick}
+            />
+          </div>
+      <div className='col-md-7'>
+        {this.state.list.length > 0 && this.state.list.map((list) => {
+        let priority
+        if (list.priority == 1) {
+          priority = 'alert-success'
+        } else if (list.priority == 2) {
+          priority = 'alert-warning'
+        } else if (list.priority == 3) {
+          priority = 'alert-danger'
+        }
+
+    return (
+      <ToDoList
+        id={list.id}
+        text={list.text}
+        priority={priority}
+        edit={this.handleEdit}
+        delete={this.handleViewTodoDelete}
+        isEditing={list.isEditing}
+        save={this.handleSave} 
+        isComplete={this.completedTask}
+        handleNewSave={this.handleNewSave}
+        clickEdit={this.clickEdit}
+        clickDelete={this.clickDelete}
+        viewTodoEdit={this.viewTodoEdit}
+        viewToDoPriority={this.viewToDoPriority}
+        // completedTask={this.completedTask}
+      />
+      )
+      })}
       </div>
+    </div>
+  </div>
+</div>
     )
   }
 }
 
 export default App
+
--- ToDo.jsx
+++ ToDo.jsx
@@ -1,17 +1,51 @@
 import React from 'react'
+		
+class ToDo extends React.Component {
+render() {
+	return (
+		<div className='left-section'>
+		  <div className='card'>
+		  <div className='card-header'>
+		    Add New Todo
+		  </div>
+		  </div>
 
-function ToDo(props) {
-
-  return (
-        <li type='checkbox'>
-            {props.item}
-
-              <button className="action" name='delete' onClick={props.handleRemove}>
-                Delete
-              </button>
-             
-        </li>
-  )
+		  <div className='form-group'>
+		  <div className='card-body'>
+		    <label>
+          </label>
+          <textarea 
+            className='container-input' 
+            name='text' 
+            id='text area' rows='3' 
+            onChange={this.props.handleChange}/>
+        <br/>
+		      <div className='row'/>
+            <label>
+              Level of importance
+            </label>		
+              <select 
+                name='priority' 
+                className='create-todo-priority' 
+                onChange={this.props.handleChange}>
+                <option value='0'>Priority</option>
+                <option value='1'>Low</option>
+                <option value='2'>Medium</option>
+                <option value='3'>High</option>
+              </select>
+            <br/>
+              <button 
+            className='action1' 
+            onClick={this.props.handleClick}>
+              Add 
+          </button>
+          <hr/>
+		      </div>
+		    </div>
+		  </div>
+		)
+	}
 }
 
 export default ToDo
+
